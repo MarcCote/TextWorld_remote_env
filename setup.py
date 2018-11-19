202299dd128d439da56056d3bc20503ce3d6e849
SP Mohanty <spmohanty91@gmail.com>
2018-11-19 02:48:35 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='textworld-remote-env',
-    version='0.1.0',
+    version='0.1.1',
     license='Apache Software License 2.0',
     description='Remote Env for TextWorld',
     long_description='%s\n%s' % (
